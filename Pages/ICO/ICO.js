503e4cb52e6fafe91af56a06ad569ef6ee6803d8
Line Ibo <ibo.line@hotmail.fr>
2024-06-27 17:01:16 +0200

    modifications des noms de class + plusieurs légères modifications pour un rendu plus proche de la maquette + le css pour la section d'achat de token est terminée + le bouton d'achat change selon la somme

--- Pages/ICO/ICO.js
+++ Pages/ICO/ICO.js
@@ -1,4 +1,6 @@
-const connectWalletButton = document.querySelector(".wallet-connexion_button");
+const connectWalletButton = document.querySelector(
+  ".invest__section__connect-wallet__button"
+);
 
 connectWalletButton.addEventListener("click", async () => {
   if (typeof window.ethereum !== "undefined") {
@@ -12,59 +14,87 @@ connectWalletButton.addEventListener("click", async () => {
         const accountAddress = accounts[0];
         console.log("Connected account:", accountAddress);
 
-        // Changement du texte du bouton
-        connectWalletButton.textContent = "investir";
-
         // Chanchement du nom de la class pour plus de compréhension
         const walletInvestSection = document.querySelector(
-          ".wallet-connexion_text"
+          ".invest__section__connect-wallet__text"
         );
-        walletInvestSection.classList.remove("wallet-connexion_text");
-        walletInvestSection.classList.add("wallet_invest-section");
+        walletInvestSection.classList.remove(
+          "invest__section__connect-wallet__text"
+        );
+        walletInvestSection.classList.add("invest__section__connected-wallet");
 
-        //apparition des boutons de transaction et déconnection
+        // apparition des boutons de transaction et déconnection
         const connectedWalletButtons = document.querySelector(
-          ".connected-wallet_buttons"
+          ".invest__section__buttons"
         );
         connectedWalletButtons.style.visibility = "visible";
 
-        //----------- Ajout de la section d'achat
+        //----------- Ajout de la section d'achat de token
         walletInvestSection.innerHTML = `
-       <div class="wallet-transaction_investment">
-        <div class="choose-crypto_wrapper">
-          <p>Vous investissez en :</p>
-          <div class="choose-crypto-input input">
-            <p>Matic</p>
-            <div class="choose-crypto-input_right-side_wrapper">
-              <div class="current-crypto-value">
-                <span>1921</span>
-                <span class="current-crypto-value_value-font-style">≈$1920</span>
-              </div>
-              <span class="line"></span>
-              <span class="down-arrow-img">
-                <img
-                  src="../../assets/images/down-arrow-img.png"
-                  alt="flèche blanche pointant vers le bas"
-              /></span>
+       <div class="connected-wallet__token-purchase">
+      <div class="connected-wallet__select-crypto__wrapper">
+        <p>Vous investissez en :</p>
+        <div class="select-crypto__input input">
+          <p>Matic</p>
+          <div class="select-crypto__input__wrapper">
+            <div class="select-crypto__input__current-value__wrapper">
+              <span>1921</span>
+              <span class="select-crypto__input__current-value--size">≈$1920</span>
             </div>
+            <span class="line"></span>
+            <span class="select-crypto__input__down-arrow">
+              <img
+                src="../../assets/images/down-arrow-img.png"
+                alt="flèche blanche pointant vers le bas"
+            /></span>
           </div>
         </div>
-        <div class="choose-amount_wrapper">
-          <p>Montant investi :</p>
-          <div class="choose-amount_input input">
-            <p>0</p>
-            <div class="choose-amount_input_right-side_wrapper">
-              <span>≈150</span>
-              <span>MAX</span>
-            </div>
+      </div>
+      <div class="money-to-invest__input input">
+          <input
+            type="number"
+            value="0"
+            class="input__field"
+          />
+          <div class="money-to-invest__input__wrapper">
+            <span class="money-to-invest__input__wrapper--size">≈150</span>
+            <button class="money-to-invest__input__wrapper__button">MAX</button>
           </div>
         </div>
-      </div>`;
+</div>
+    </div>
+    <div class="connected-wallet__token-amount">
+      <p>Vous recevez en $ANZ</p>
+      <div class="token-amount__wrapper">
+        <p>0</p>
+        <img src="../../assets/images/TOKEN-color.png" alt="Logo du jeux Anazir sous forme d'une piece de monnaie" />
+      </div>
+      <div class="token-amount__max-amount__wrapper">
+       <span class="token-amount__max-amount__line"></span>     
+        <p class="token-amount__max-amount">15 000 000</p>
+        <p class="token-amount__max-amount__info">Token alloués pressed</p>
+      </div>
+    </div>`;
 
         //-----------
-      } else {
-        console.warn("No accounts found.");
+
+        // Changement du texte du bouton
+        connectWalletButton.textContent = "Ticket minimum : 500$";
+        const moneyInvestInput = document.querySelector(".input__field");
+
+        moneyInvestInput.addEventListener("input", () => {
+          const inputValue = moneyInvestInput.value;
+
+          if (!isNaN(inputValue) && inputValue >= 500) {
+            connectWalletButton.textContent = "investir";
+          } else {
+            connectWalletButton.textContent = "Ticket minimum : 500$";
+          }
+        });
       }
+      //------------
+
+      //------------ Déconnection de l'utilisateur ??
     } catch (error) {
       console.error("User denied account access:", error);
     }
